f554a72d284fc6c40a5070ea34b7329bbab9404d
MATHEUS DE SOUZA MATOS <matheus.smatos11@senacsp.edu.br>
2025-03-11 20:05:54 -0300

    fix: CadastroEndereco

---
+++ WFCadastroEndereco/FormCadastro.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WFCadastroEndereco
+{
+    partial class FormCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastro));
+            lblNomeCompleto = new Label();
+            txtNomeCompleto = new TextBox();
+            btnSalvar = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            SuspendLayout();
+            // 
+            // lblNomeCompleto
+            // 
+            lblNomeCompleto.AutoSize = true;
+            lblNomeCompleto.Location = new Point(26, 24);
+            lblNomeCompleto.Name = "lblNomeCompleto";
+            lblNomeCompleto.Size = new Size(102, 15);
+            lblNomeCompleto.TabIndex = 0;
+            lblNomeCompleto.Text = "Nome Completo :";
+            // 
+            // txtNomeCompleto
+            // 
+            txtNomeCompleto.Location = new Point(26, 42);
+            txtNomeCompleto.Name = "txtNomeCompleto";
+            txtNomeCompleto.Size = new Size(377, 23);
+            txtNomeCompleto.TabIndex = 1;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Image = (Image)resources.GetObject("btnSalvar.Image");
+            btnSalvar.Location = new Point(296, 419);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(107, 38);
+            btnSalvar.TabIndex = 8;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Cep :";
+            // 
+            // label2
+            // 
+            label2.BorderStyle = BorderStyle.Fixed3D;
+            label2.Location = new Point(26, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(377, 1);
+            label2.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(26, 194);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(377, 23);
+            textBox2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 176);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 15);
+            label3.TabIndex = 12;
+            label3.Text = "Nome Completo :";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(26, 271);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(377, 23);
+            textBox3.TabIndex = 15;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(26, 253);
+            label4.Name = "label4";
+            label4.Size = new Size(102, 15);
+            label4.TabIndex = 14;
+            label4.Text = "Nome Completo :";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(26, 348);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(377, 23);
+            textBox4.TabIndex = 17;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(26, 330);
+            label5.Name = "label5";
+            label5.Size = new Size(102, 15);
+            label5.TabIndex = 16;
+            label5.Text = "Nome Completo :";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(28, 121);
+            maskedTextBox1.Mask = "00000-999";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(100, 23);
+            maskedTextBox1.TabIndex = 18;
+            // 
+            // FormCadastro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(467, 478);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnSalvar);
+            Controls.Add(txtNomeCompleto);
+            Controls.Add(lblNomeCompleto);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FormCadastro";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FormCadastro";
+            Load += FormCadastro_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNomeCompleto;
+        private TextBox txtNomeCompleto;
+        private Button btnSalvar;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private MaskedTextBox maskedTextBox1;
+    }
+}
